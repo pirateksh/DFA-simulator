7ad07f31d81184d6d56f34b357d2a19d2de32f9e
Kshitiz Srivastava <ksh1998@gmail.com>
2020-01-16 16:39:46 +0530

    readme updated

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # DFA-simulator
-A command line simulator for Deterministic Finite Automata (DFA) 
+A command line simulator for <b>_Deterministic Finite Automata_</b> (DFA).
+
+## How to use ?
+* Clone this repository into a folder using <b>git clone</b>.
+* Open terminal and compile the _dfa.cpp_ file. (in Linux terminal use, <b>g++ dfa.cpp</b> to compile).
+* Run the generated _a.out_ file using <b>./a.out</b>.
+* Firstly, you need to configure the DFA.
+
+## Configuring DFA
+* <b>Set of inputs: </b> Firstly, enter number of inputs and then enter all the inputs separated by a space.
+* <b>Set of states: </b> You just need to enter total number of states (say n) in your DFA. By default states are numbered from <b>_0_</b> to <b>_n-1_</b>.
+* <b>Set of final states: </b> Firstly, enter number of final states and then enter all the final states (remember set of states are numbered from 0 to n-1, so only enter numbers between 0 and n-1 included) present in your DFA.
+* <b>Initial State :</b> It is assumed to be 0 by default.
+* <b>Transition function :</b> For each state, starting from initial state, and for each input per state, fill the corresponding next states.
+* Sit back and relax, your DFA is now configured and ready to be used.
+
+## Testing DFA
+* Test inputs can be written in _input.txt_ file separated by a new line character i.e. one input string per line. The configured DFA will automatically test these input strings which are specified in this file.
+* If you feel like testing on more inputs after the inputs from file are exhausted, you can even give input strings from the command line itself.
+
+## Output Format 
+* For each input string, the DFA will specify whether the input was <b>Accepted</b>, <b>Rejected</b> or <b>Invalid Input</b>.
+
+## Contributor
+* <a href="https://github.com/pirateksh/">Kshitiz Srivastava</a>
---
+++ dfa.cpp
@@ -0,0 +1,136 @@
+/* 
+	@author: Kshitiz Srivastava (pirateksh)
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+#define vi vector<int>
+#define si set<char>
+#define pb(v, x) v.push_back(x)
+
+
+vector<char> set_of_inputs;
+int initial_state;
+int number_of_states;
+vi set_of_final_states;
+vi transition_function[100000];
+map<char, int> input_to_index;
+
+void enter_set_of_inputs() {
+	int n;
+	char input;
+	cout << "Enter total number of inputs: ";
+	cin >> n;
+	cout << "Enter inputs separated by a space: ";
+	for(int i = 0; i < n; i++) {
+		cin >> input;
+		pb(set_of_inputs, input);
+		input_to_index[input] = i+1;
+	}
+	cout << "Set of inputs have been successfully recorded.\n";
+}
+
+void enter_total_number_of_states() {
+	cout << "Enter total number of states: ";
+	cin >> number_of_states;
+	cout << "Set of states have been successfully recorded.\n";
+}
+
+void enter_set_of_final_states() {
+	int n, final_state;
+	cout << "Enter total number of final states: ";
+	cin >> n;
+	cout << "Enter final states separated by a space: ";
+	for(int i = 0; i < n; i++) {
+		cin >> final_state;
+		pb(set_of_final_states, final_state);
+	}
+	cout << "Set of final states have been successfully recorded.\n";
+}
+
+void enter_initial_state() {
+	cout << "Enter initial state: ";
+	cin >> initial_state;
+	cout << "Initial state has been successfully recorded.\n";
+}
+
+void define_transition_function() {
+	int number_of_inputs = set_of_inputs.size(), next_state;
+	for(int i = 0; i < number_of_states; i++) {
+		for(int j = 0; j < number_of_inputs; j++) {
+			cout << "Current state: " << i << " Input: " << set_of_inputs[j] << " Enter next state: ";
+			cin >> next_state;
+			pb(transition_function[i], next_state);
+		}
+	}
+}
+
+void configure_machine() {
+	enter_set_of_inputs();
+	enter_total_number_of_states();
+	enter_set_of_final_states();
+	enter_initial_state();
+	define_transition_function();
+	cout << "Machine has been successfully configured according to your input(s).\nYou can use this machine now\n";
+}
+
+
+
+void init() {
+	cout << "----------------------------------\n";
+	cout << " ### Welcome to DFA Simulator ###\n";
+	cout << "----------------------------------\n";
+}
+
+void check(string s) {
+	int current_state = initial_state, i;
+	for(i = 0; i < s.length(); i++) {
+		if(input_to_index[s[i]] == 0) break;
+		current_state = transition_function[current_state][input_to_index[s[i]] - 1];
+ 	}
+ 		
+ 	if(i!=s.length()) {
+ 		cout << s << " is Invalid Input\n";
+ 	}
+ 	else {
+	 	for(i = 0; i < set_of_final_states.size(); i++) {
+	 		if(current_state == set_of_final_states[i]) break;
+	 	}
+	 	
+	 	if(i == set_of_final_states.size()) cout << s << " is Rejected\n";
+	 	else cout << s << " is Accepted\n";
+	 }
+}
+
+void test() {
+	fstream file; 
+    string s, filename; 
+    filename = "input.txt"; 
+  
+    file.open(filename.c_str()); 
+  	cout << "Testing on pre-fed inputs from file:\n";
+    while (file >> s) { 
+        check(s);
+    }
+    cout << "All inputs from the file have been exhausted.\n";
+    file.close();
+    int flag;
+    cout << "Do you want to test some more inputs ? (enter 1 for Yes): ";
+    cin >> flag;
+    while(flag==1) {
+    	cout << "Enter input string: ";
+    	cin >> s;
+    	check(s);
+    	cout << "Do you want to test some more inputs ? (enter 1 for Yes): ";
+    	cin >> flag;
+    }
+
+    cout << "Thankyou for using this simulator.\n";
+}
+
+int main() {
+	init();
+	configure_machine();
+	test();
+	return 0;
+}
